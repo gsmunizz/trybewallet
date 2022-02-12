6f5dc069228ce4b4d43993ab1d813fc45bcdbc67
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2022-02-12 17:34:23 -0300

    Requisito 1 finalizado

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 // Coloque aqui suas actions
+export const LOG_IN = 'LOG_IN';
+export const GET_WALLET = 'GET_WALLET';
+
+export default function loginAction(payload) {
+  return {
+    type: LOG_IN,
+    payload,
+  };
+}
+
+export const getWallet = () => ({
+  type: GET_WALLET,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import { connect } from 'react-redux';
+// import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import user from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailInput: '',
+      passwordInput: '',
+    };
+  }
+
+  handleInputChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  // Para a validação do emil, busquei a regex no seguinte tópico:
+  // https://pt.stackoverflow.com/questions/1386/express%c3%a3o-regular-para-valida%c3%a7%c3%a3o-de-e-mail#1389
+  handleCheckInput = (email, pwd) => {
+    const MIN_LENGTH = 6;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+    const verifyEmail = emailRegex.test(email);
+    const verifyPassword = pwd.length >= MIN_LENGTH;
+
+    const verify = !(verifyEmail && verifyPassword);
+
+    return verify;
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { emailInput } = this.state;
+    const { submitEmail, history } = this.props;
+
+    submitEmail(emailInput);
+
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { emailInput, passwordInput } = this.state;
+
+    return (
+      <section>
+        <input
+          data-testid="email-input"
+          type="text"
+          name="emailInput"
+          value={ emailInput }
+          onChange={ this.handleInputChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="passwordInput"
+          value={ passwordInput }
+          onChange={ this.handleInputChange }
+        />
+        <button
+          data-testid="login-submit-button"
+          type="submit"
+          disabled={ this.handleCheckInput(emailInput, passwordInput) }
+          onClick={ (e) => {
+            this.handleSubmit(e);
+          } }
+        >
+          Entrar
+        </button>
+      </section>);
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  submitEmail: (email) => dispatch(user(email)),
+});
+
+Login.propTypes = {
+  submitEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
