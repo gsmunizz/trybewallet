9324a408b578f151c87c62a12832c3a1e95e35b6
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2022-02-17 13:41:04 -0300

    Requisito 06 finalizado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import InputExpenses from '../components/InputExpenses';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
   render() {
+    const { expenses } = this.props;
     return (
       <>
         <Header />
         <InputExpenses />
+
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+
+        </table>
+
       </>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+Wallet.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
